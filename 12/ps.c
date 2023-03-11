2ce00f3f0260657a32cc961aecbb58a95408bde6
idosh <idoshziv@gmail.com>
2023-03-11 16:51:19 +0200

    adding second maman

---
+++ 12/ps.c
@@ -0,0 +1,16 @@
+// Ido Ziv - 318623428
+// logic of ps
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int
+main(int argc, char *argv[])
+{
+// Add here what needed
+
+  cps128();
+
+  exit(1);
+}
