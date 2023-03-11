ad389a17c21c6c2f664a431f2951718020fb42fd
idosh <idoshziv@gmail.com>
2023-03-11 16:51:02 +0200

    adding first maman

---
+++ 11/ps.c
@@ -0,0 +1,14 @@
+// Ido Ziv - 318623428
+// logic of ps 
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int
+main(int argc, char *argv[])
+{
+cps128();
+
+exit();
+}
